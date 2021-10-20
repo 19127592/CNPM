1c7b947fcc672233b52b65e72a703560ac807362
letri-hub <61229133+letri-hub@users.noreply.github.com>
2021-10-20 23:19:59 +0700

    Update

--- client/src/components/mainpage/Pages.js
+++ client/src/components/mainpage/Pages.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import {Switch,Route} from 'react-router-dom'
+import Products from './products/Products'
+import Login from './auth/Login'
+import Register from './auth/Register'
+import Cart from './cart/Cart'
+import NotFound from './ultilis/not_found/NotFound'
 export default function Pages() {
     return (
-        <div>
-            Main pages components
-        </div>
+        <Switch>
+            <Route path='/' exact component={Products}/>
+            <Route path='/login' exact component={Login}/>
+            <Route path='/register' exact component={Register}/>
+            <Route path='/cart' exact component={Cart}/>
+
+            <Route path='*' exact component={NotFound}/>
+        </Switch>
     )
 }
